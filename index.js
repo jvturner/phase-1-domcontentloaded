bd5173055d5850359058e6e4f4750c81fd922955
Jamond Turner <jamondturner@yahoo.com>
2022-04-27 21:33:29 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("The DOM has loaded");
+    const newText = document.getElementById('text');
+    newText.textContent = "This is really cool!"
+    console.log(newText);
+
+
+});
+
+
+
+
+
+
+
+// newText.textContent("This is really cool!");
+
+
+
+
+
+
+
+
